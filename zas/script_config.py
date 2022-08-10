2ac3c570e6e410325d9deeaaf76fdd81bf091d6b
Lemon65 <aaron.schoettger@gmail.com>
2022-08-10 01:09:31 -0700

    Config change

--- zas/script_config.py
+++ zas/script_config.py
@@ -3,7 +3,7 @@ import os
 # This is a configuration file for the script
 
 # This is how often the script will back up your saves 300sec == 5mins
-SAVE_INTERVAL_SEC = 10
+SAVE_INTERVAL_SEC = 300
 
 # The save location where you want to save the backups, note this needs to be the "FULL PATH" to the target folder
 BACKUP_SAVE_PATH = os.path.expanduser('~\\Zomboid\\') + "zas_backup_saves"
